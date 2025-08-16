d11b106e75f82de53a6abae37b4bb9cbea4fc652
ClintOSD <113477414+ClintOSD@users.noreply.github.com>
2025-08-16 17:29:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
+# Importa la clase Flask del módulo flask
 from flask import Flask
+
+# Crea una instancia de la aplicación Flask
 app = Flask(__name__)
 
+# Define una ruta para la página de inicio
 @app.route('/')
 def hello_world():
-    return 'Hola ITLA - Un enorme saludo desde mi casa!'
+    # Retorna el mensaje "¡Hola, Mundo!"
+    return '¡Hola, Mundo!'
 
+# Este bloque se ejecuta solo si el script se corre directamente
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    # Ejecuta la aplicación en modo de depuración
+    # El host '0.0.0.0' hace que la aplicación sea accesible desde fuera del contenedor Docker
+    app.run(host='0.0.0.0', debug=True)
